a6b48cdddbf9e42832ad3c027b3cd047e8fcd959
David Rodríguez Elipe <egoz3ntrum@gmail.com>
2023-03-04 17:40:41 +0100

    Inception of inceptions

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Inception-of-Things
+
+
+To start, export the VAGRANT_HOME env as /goinfre/darodrig/.vagrant.d
+
+USER=darodrig
+```
+mkdir -p /goinfre/$USER/.vagrant.d
+mkdir -p /goinfre/$USER/.vbox
+ln -s $HOME/VirtualBox\ VMs /goinfre/$USER/.vbox
+VAGRANT_HOME=/goinfre/$USER/.vagrant.d vagrant up
+vagrant ssh iot
+```
+
+To get the ssh-config file, run the following command:
+
+```
+vagrant ssh-config iot
+```
+
+It can be copied to your local .ssh/config file to allow you to connect to the VM using the following command:
+
+```
+ssh iot
+```
--- p1/scripts/deps.sh
+++ p1/scripts/deps.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 set -e
 
+apt-get update && apt-get upgrade -y >/dev/null
 
-DEPENDENCIES=(lynx wget gnupg ruby-full curl qemu-kvm libvirt-daemon-system ncdu)
+DEPENDENCIES=(lynx wget gnupg curl ncdu linux-headers-amd64 linux-headers-4.19.0-21-amd64)
 
 # Install dependencies
 for dep in "${DEPENDENCIES[@]}"; do
     if [ -z "$(which $dep)" ]; then
         echo "Installing $dep..."
-        apt-get install -y $dep >/dev/null
+        apt-get install -yq $dep >/dev/null
         echo -e [+] "\e[32m$dep installed successfully!\e[0m"
     else
         echo -e [+] "\e[32m$dep already installed.\e[0m"
@@ -20,9 +21,9 @@ done
 if [ -z "$(which virtualbox)" ]; then
     echo "Installing virtualbox..."
     if [ ! -f /tmp/virtualbox.deb ]; then
-        wget https://download.virtualbox.org/virtualbox/7.0.6/virtualbox-7.0_7.0.6-155176~Debian~bullseye_amd64.deb -O /tmp/virtualbox.deb  2>&1
+        wget https://download.virtualbox.org/virtualbox/7.0.6/virtualbox-7.0_7.0.6-155176~Debian~buster_amd64.deb -O /tmp/virtualbox.deb  2>&1
     fi
-    apt install -yq /tmp/virtualbox.deb  2>&1
+    apt-get install -y /tmp/virtualbox.deb >/dev/null
     echo -e [+] "\e[32mVirtualbox installed successfully!\e[0m"
     # Clean up
     rm /tmp/virtualbox.deb >/dev/null 2>&1 && echo -e [+] "\e[32mCleaned up successfully!\e[0m" || true
@@ -36,7 +37,9 @@ if [ -z "$(which vagrant)" ]; then
     echo "Installing vagrant..."
     wget -O- https://apt.releases.hashicorp.com/gpg | gpg --dearmor | tee /usr/share/keyrings/hashicorp-archive-keyring.gpg
     echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | tee /etc/apt/sources.list.d/hashicorp.list
-    apt update && apt-get install -yq vagrant
+    apt update && apt-get install -yq vagrant >/dev/null
+    # add vagrant user to vboxusers group
+    /sbin/usermod -aG vboxusers vagrant
     echo -e [+] "\e[32mVagrant installed successfully!\e[0m"
 else
     echo -e [+] "\e[32mVagrant already installed.\e[0m"
