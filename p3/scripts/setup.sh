31cc6a1919d3d91d532b9f374ab718311a2464e6
David Rodríguez Elipe <egoz3ntrum@gmail.com>
2023-04-06 12:28:11 +0200

    argocd funchiona

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -12,6 +12,14 @@ else
     sleep 0.2
 fi
 
+if [ -z $(which argocd) ]; then
+    curl -sSL -o argocd-linux-amd64 https://github.com/argoproj/argo-cd/releases/latest/download/argocd-linux-amd64
+    sudo install -m 555 argocd-linux-amd64 /usr/local/bin/argocd
+    rm argocd-linux-amd64
+else 
+    echo [+] ArgoCD already installed.
+fi
+
 if [ -z "$(which k3d)" ]; then
     echo "Installing k3d and kubectl..."
     wget -q -O - https://raw.githubusercontent.com/k3d-io/k3d/main/install.sh | sudo bash
@@ -21,10 +29,8 @@ if [ -z "$(which k3d)" ]; then
     echo 'alias k=kubectl' >>~/.bashrc
     echo 'source <(kubectl completion bash)' >>~/.bashrc
     echo 'source <(kubectl completion bash)' >>/root/.bashrc
+    rm kubectl
     kubectl completion bash >/etc/bash_completion.d/kubectl
-
-
-
     echo [+] K3d and kubectl installed successfully!
 else
     echo [+] K3d already installed.
@@ -33,7 +39,7 @@ fi
 
 if [ -z "$(k3d cluster list | grep iot)" ];then
     echo "Creating k3d cluster..."
-    k3d cluster create  --port 8888:8888@loadbalancer  --port 80:80@loadbalancer iot
+    k3d cluster create  --port 8888:8888@loadbalancer  --port 80:80@loadbalancer iot --agents 1 --servers 1 --wait
     echo [+] K3d cluster created successfully!
 else
     echo [+] K3d cluster already created.
--- p3/scripts/up.sh
+++ p3/scripts/up.sh
@@ -3,10 +3,33 @@
 
 #namespaces
 
-kubectl apply -f /home/vagrant/iot/p3/confs/argoCD/namespace.yml
-kubectl apply -f /home/vagrant/iot/p3/confs/argoCD/argocd.depl.yml
+kubectl apply -f /home/vagrant/iot/p3/confs/argoCD/namespace.yml -n argocd
+kubectl config set-context --current --namespace=argocd
+echo [+] Namespace created successfully!
+sleep 1
 
+kubectl apply -f /home/vagrant/iot/p3/confs/argoCD/install.yml -n argocd
+echo [+] ArgoCD deployment created successfully!
+
+kubectl wait -n argocd --for=condition=Ready pods --all
+echo [+] ArgoCD pods are ready!
+kubectl apply -f /home/vagrant/iot/p3/confs/argoCD/ingress.yml -n argocd
+echo [+] ArgoCD ingress created successfully!
+
+kubectl patch svc argocd-server -n argocd -p '{"spec": {"type": "LoadBalancer"}}'
+echo [+] ArgoCD service created successfully!
+sleep 0.2
+
+# kubectl port-forward svc/argocd-server -n argocd 8080:443
+echo 127.0.0.1 argocd.local >> /etc/hosts
+argocd login --insecure --core argocd.local
+argocd admin initial-password -n argocd --insecure | tee /home/vagrant/iot/p3/confs/argoCD/argocd.passwd
+echo ArgoCD password created successfully!
+echo Password:
+head -n 1 /home/vagrant/iot/p3/confs/argoCD/argocd.passwd
+
+argocd login --insecure --core --username admin --password $(head -n 1 /home/vagrant/iot/p3/confs/argoCD/argocd.passwd) argocd.local
 
 # kubectl apply -f /home/vagrant/iot/p3/confs/dev/namespace.yml -n dev
 # kubectl apply -f /home/vagrant/iot/p3/confs/dev/deployment.yml -n dev
-kubectl apply -f /home/vagrant/iot/p3/confs/ingress.yml -n dev
+# kubectl apply -f /home/vagrant/iot/p3/confs/ingress.yml
